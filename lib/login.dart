1a1a57e09ca888bf6fc968a648ba9637f30c8fba
fanorflores <fanorflores@gmail.com>
2019-11-10 03:29:01 -0600

    style: Agregada capa visual a los controles de login
    En este apartado se deja caer la capa visual sonbre los componentes de la pantalla, únicamente se coloca la capa visual y la Navvegación

--- lib/login.dart
+++ lib/login.dart
@@ -10,45 +10,75 @@ class FirtsQ extends StatefulWidget
 }
 
 class _FirtsQState extends State<FirtsQ> {
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
 
+
       body: new Container(
-        color: Colors.blueAccent,
+        color: Color(0xff39414E),
 
         child: new Center(
-
           child: Builder(
+
             builder: (context) =>(
                 new Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
+
                   children: <Widget>[
-                    new Center(
+                    new Container(
+                      padding: EdgeInsets.only(top: 50.0),
 
                       child: new Column(
-                        children: <Widget>[
+
+                          children: <Widget>[
+                                GestureDetector(
+                                    onTap: () {
+
+                                },
+                                child: new Image(
+                                image: AssetImage("assets/logo-noni.png"),
+                                width: 250.0
+
+                          )),
+                            new Container
+                              (
+                              child: new Container(
+                                padding: new EdgeInsets.only(top:35.0),
+                                  child: new Text("Iniciar Sesión",style: new TextStyle(fontSize: 24.0,color: Colors.white),)
+                              ),
+                            )
 
                         ],
                       ),
                     ),
-                    new Card(
+                    new Container(
                       child: new Container(
-                        padding: new EdgeInsets.all(30),
+                        padding: new EdgeInsets.only(bottom: 30,left: 30.0, right: 30.0),
                         child: new Column(
                           children: <Widget>[
-                            new TextField(
+                            new Theme(
+                                data: new ThemeData(
+                                    primaryColor: Colors.white,
+                                    accentColor: Colors.white,
+                                    hintColor: Colors.white,
+
+                                ),
+                                child: new TextField(
                               decoration: new InputDecoration(
-                                  hintText: "Nombre Usuario",
-                                  icon: new Icon(Icons.person)
+                                hintText: "Nombre Usuario",
+                                icon: new Icon(Icons.person,color: Colors.white,),
                               ),
-                            ),
+
+                            )),
                             new TextField(
+                              cursorColor: Colors.white,
                               obscureText: true,
                               decoration: new InputDecoration(
                                   hintText: "Contraseña",
-                                  icon: new Icon(Icons.lock)
+                                  icon: new Icon(Icons.lock,color: Colors.white,),
                               ),
 
                             ),
@@ -56,7 +86,7 @@ class _FirtsQState extends State<FirtsQ> {
                                 onTap: (){
                                   Scaffold.of(context).showSnackBar(
                                       new SnackBar(
-                                        content: new Text("Te enviaremos correo con tu contraseña"),
+                                        content: new Text("Te enviaremos correo con tu contraseña",style: new TextStyle(color: Color(0xff008FC9) ),),
                                         duration: new Duration(seconds: 5),
 
                                       )
@@ -67,7 +97,7 @@ class _FirtsQState extends State<FirtsQ> {
                                 child:
                                 new Container(
                                   padding: new EdgeInsets.only(top:15.0),
-                                  child: new Text("¿Olvidaste la contraseña?",style: new TextStyle(fontSize: 14.0),textAlign: TextAlign.right),
+                                  child: new Text("¿Olvidaste la contraseña?",style: new TextStyle(fontSize: 14.0,color: Color(0xff008FC9)),textAlign: TextAlign.right),
                                 )
                             ),
 
@@ -75,10 +105,9 @@ class _FirtsQState extends State<FirtsQ> {
                               minWidth: 200.0,
                               child: new RaisedButton(
 
-                                child: new Text("Iniciar"),
+                                child: new Text("Iniciar",style: new TextStyle(fontSize: 16.0),),
                                   textColor: Colors.white,
-                                  color: Colors.blueAccent,
-
+                                  color: Color(0xff5ACA2A),
 
                                   onPressed: (){}),
 
@@ -95,9 +124,42 @@ class _FirtsQState extends State<FirtsQ> {
                               child: new Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: <Widget>[
-                                  new Icon(Icons.brightness_auto,size: 55.0,),
-                                  new Icon(Icons.person,size: 55.0,),
-                                  new Icon(Icons.gamepad,size: 55.0,),
+                                  GestureDetector(
+                                  onTap: () {
+
+                            },
+                                child: new Container(
+                                  padding: EdgeInsets.only(right: 10.0),
+                                    child: new Image(
+                                      image: AssetImage("assets/btn-fb.png"),
+                                      width: 50.0,
+
+                                    )
+                                )),
+                                  GestureDetector(
+                                      onTap: () {
+
+                                      },
+                                      child:
+                                  new Container(
+                                    padding: EdgeInsets.only(right: 10.0),
+                                    child: new Image(
+                                        image: AssetImage("assets/btngt.png"),
+                                        width: 50.0
+
+                                    ),
+                                  ))
+                                  ,
+                                  GestureDetector(
+                                      onTap: () {
+
+                                      },
+                                      child: new Image(
+                                          image: AssetImage("assets/btntwt.png"),
+                                          width: 50.0
+                                      )),
+
+
                                 ],
                               ),
                             ),
@@ -108,11 +170,12 @@ class _FirtsQState extends State<FirtsQ> {
                     ),
                     new Container(
 
+
                       child: new Column(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: <Widget>[
-                          new Text("Términos y Condiciones",style: new TextStyle(fontSize: 14.0),textAlign: TextAlign.center,),
+                          new Text("Términos y Condiciones",style: new TextStyle(fontSize: 14.0,color: Color(0xff008FC9)),textAlign: TextAlign.center),
                         ],
                       ),
 
